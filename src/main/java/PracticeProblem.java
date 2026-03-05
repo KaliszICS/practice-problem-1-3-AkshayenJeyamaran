8fd524c0882a149abf6e0e849e8b950c8cf02782
AkshayenJeyamaran <akshayen.jeyamaran@student.tdsb.on.ca>
2026-03-05 01:30:10 +0000

    Finish questions 1 and 2

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 public class PracticeProblem {
+    public static String readFile(String filename) {
+        BufferedReader reader = null;
+        String line = null;
+        String content = "";
 
-	public static void main(String args[]) {
-
-	}
-
-	
-
+        try {
+            reader = new BufferedReader(new FileReader(filename));
+            while ((line = reader.readLine()) != null) {
+                content += line + "\n";
+            }
+            reader.close();
+        }
+        catch (IOException e) {
+            System.out.println("Error reading file.");
+		}
+        return content;
+    }
+	public static String backwardsReadFile(String filename) {
+    	BufferedReader reader = null;
+    	String line = null;
+    	String content = "";
+   		 try {
+        	reader = new BufferedReader(new FileReader(filename));
+        	while ((line = reader.readLine()) != null) {
+            	content += line + "\n";
+        	}
+        	reader.close();
+    		}
+    	catch (Exception e) {
+        	System.out.println("Error reading file.");
+    		}
+    	String backwards = "";
+   		for (int i = content.length() - 1; i >= 0; i--) {
+        	backwards += content.charAt(i);
+    	}
+    	return backwards;
+}
 }
